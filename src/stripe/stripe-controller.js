cfb6a3d1c04b43b7277426dfb5f23731bc8c33c4
malerba423 <malerba423@gmail.com>
2021-04-01 22:54:50 -0700

    adding code to get stripe terminal connection token

--- src/stripe/stripe-controller.js
+++ src/stripe/stripe-controller.js
@@ -1,16 +1,50 @@
-const { STRIPE_PUBLIC_KEY } = require('../config')
-const stripeService = require('./stripe-service');
+const { STRIPE_PUBLIC_KEY } = require("../config");
+const stripeService = require("./stripe-service");
 
-exports.createPaymentIntent = async function(req, res) {
-  const { items, currency } = req.body;
+exports.getConnectionToken = async function (req, res) {
   try {
-    const paymentIntent = await stripeService.createPaymentIntent( { items, currency } );
+    let connectionToken = await stripeService.getConnectionToken();
+    res.send({ secret: connectionToken.secret });
+  } catch (e) {
+    return res.status(500).json({
+      message: "An error occured during the getConnectionToken process",
+    });
+  }
+};
+
+exports.createPaymentIntent = async function (req, res) {
+  const { items } = req.body;
+  try {
+    const amount = stripeService.calculateOrderAmount(items);
+    const paymentIntent = await stripeService.createPaymentIntent({ amount });
     // Send public key and PaymentIntent details to client
     res.send({
-        publicKey: STRIPE_PUBLIC_KEY,
-        clientSecret: paymentIntent.client_secret
+      publicKey: STRIPE_PUBLIC_KEY,
+      clientSecret: paymentIntent.client_secret,
     });
-  } catch(e) {
-    return res.status(500).json({ message: "An error occured during the createPaymentIntent process" });
+  } catch (e) {
+    return res.status(500).json({
+      message: "An error occured during the createPaymentIntent process",
+    });
   }
-}
+};
+
+exports.createPaymentIntentWithCardPresent = async function (req, res) {
+  const { items } = req.body;
+  try {
+    const amount = stripeService.calculateOrderAmount(items);
+    const paymentIntent = await stripeService.createPaymentIntentWithCardPresent(
+      { amount }
+    );
+    // Send public key and PaymentIntent details to client
+    res.send({
+      publicKey: STRIPE_PUBLIC_KEY,
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (e) {
+    return res.status(500).json({
+      message:
+        "An error occured during the createPaymentIntentWithCardPresent process",
+    });
+  }
+};
--- src/stripe/stripe-service.js
+++ src/stripe/stripe-service.js
@@ -1,19 +1,49 @@
-
-const { STRIPE_SECRET_KEY } = require('../config')
+const { STRIPE_SECRET_KEY } = require("../config");
 const stripe = require("stripe")(STRIPE_SECRET_KEY);
 
-const calculateOrderAmount = items => {
-    //look up item price in DB and use that
-    const dollarSum = items.reduce((a, b) => a + (b.price || 0), 0)
-    return dollarSum * 100; //stripe expects number of pennies, so multiply by 100
+exports.calculateOrderAmount = function (items) {
+  //TODO: look up item price in DB and use that rather than trusting price data sent from client
+  const dollarSum = items.reduce((a, b) => a + (b.price || 0), 0);
+  return dollarSum * 100; //stripe expects number of pennies, so multiply by 100
 };
 
-exports.createPaymentIntent = async function( { items, currency } )  {
-    // Create a PaymentIntent with the order amount and currency
-    const amount = calculateOrderAmount(items)
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: amount,
-        currency: currency
-    });
-    return paymentIntent;
-}
+exports.createLocation = async function () {
+  const location = await stripe.terminal.locations.create({
+    display_name: "HQ",
+    address: {
+      line1: "3814 Lakeway Dr",
+      city: "Bellingham",
+      state: "WA",
+      country: "US",
+      postal_code: "98229",
+    },
+  });
+  return location;
+};
+
+exports.createPaymentIntent = async function ({ amount }) {
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: "usd",
+  });
+  return paymentIntent;
+};
+
+exports.createPaymentIntentWithCardPresent = async function ({ amount }) {
+  // For Terminal payments, the 'payment_method_types' parameter must include
+  // 'card_present' and the 'capture_method' must be set to 'manual'
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: "usd",
+    payment_method_types: ["card_present"],
+    capture_method: "manual",
+  });
+  return paymentIntent;
+};
+
+//be sure to protect this behind authentication middleware
+exports.getConnectionToken = async function () {
+  let connectionToken = await stripe.terminal.connectionTokens.create();
+  return connectionToken;
+};
