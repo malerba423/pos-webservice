00832474a089cc5eb4c2f73b550c54198cfae538
malerba423 <malerba423@gmail.com>
2021-05-03 20:53:08 -0700

    remove unused orders repo methods

--- src/orders/orders-repo.js
+++ src/orders/orders-repo.js
@@ -8,30 +8,6 @@ exports.addNewOrder = async function ({ order }) {
   return await db('orders').insert(mapOrderGoingIntoDB(order));
 };
 
-exports.markOrderDone = async function (payload) {
-  const { orderId } = payload;
-  const orderUpdated = {
-    order_status: 'DONE',
-  };
-  return await db('orders').where({ id: orderId }).update(orderUpdated);
-};
-
-exports.markOrderCancelled = async function (payload) {
-  const { orderId } = payload;
-  const orderUpdated = {
-    order_status: 'CANCELLED',
-  };
-  return await db('orders').where({ id: orderId }).update(orderUpdated);
-};
-
-exports.markOrderNewAgain = async function (payload) {
-  const { orderId } = payload;
-  const orderUpdated = {
-    order_status: 'NEW',
-  };
-  return await db('orders').where({ id: orderId }).update(orderUpdated);
-};
-
 exports.updateOrder = async function ({ order }) {
   return await db('orders').where({ id: order.id }).update(mapOrderGoingIntoDB(order));
 };
