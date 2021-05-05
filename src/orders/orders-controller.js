8f610890a9047b07caa239bdcb9618c3ba395933
malerba423 <malerba423@gmail.com>
2021-05-05 13:09:03 -0700

    fix to get orders queue to be realtime again

--- src/orders/orders-controller.js
+++ src/orders/orders-controller.js
@@ -10,6 +10,7 @@ exports.addNewOrder = async function (req, res) {
 
     const queue = await ordersService.getActiveOrdersTodaySorted();
     io.sockets.emit('orders/current_queue', queue);
+    io.sockets.emit('orders/requery_initial_data');
   } catch (e) {
     return res.status(500).json({
       message: 'An error occured during the addNewOrder process',
