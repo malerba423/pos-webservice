d1bf8270cc029dde014f424861df7139ad8e79e7
malerba423 <malerba423@gmail.com>
2021-05-24 15:24:43 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,23 +1,29 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-module.exports = {
-  PORT: '3001',
-  NODE_ENV: 'local_development',
-  DATABASE_URL: 'postgresql://postgres:postgres@localhost:5432/postgres',
-  STRIPE_PUBLIC_KEY:
-    'pk_test_51Fa7TWKyucmbfgO2k9j41LfOVCR0YDjQDWJZsv75aaIRFCCATKhc7zBg30jKwQjOdK57yH8Qx1ts6MIxQWtTKxOx00TLgOt60u',
-  STRIPE_SECRET_KEY:
-    'sk_test_51Fa7TWKyucmbfgO2ccZajIygKaIYqz1zymEYKM7sgpaCZmP41qSeIejldlE6bEoSNnkRLg0jauV5ZsjtJq8ZKEol00YrIvFNB9',
-  SALES_TAX_RATE: '0.087',
-  JWT_SECRET: 'some_test_secret_or_something',
-  JWT_ALGORITHM: 'HS256',
-  DEV_HOST: 'trbikes.kitchen.local',
-  REACT_WEBAPP_BASE_URL: 'http://localhost:3000',
+const envVars = {
+  PORT: process.env.PORT,
+  NODE_ENV: process.env.NODE_ENV,
+  DATABASE_URL: process.env.DATABASE_URL,
+  STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY,
+  STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+  SALES_TAX_RATE: process.env.SALES_TAX_RATE,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_ALGORITHM: process.env.JWT_ALGORITHM,
+  DEV_HOST: process.env.DEV_HOST,
+  REACT_WEBAPP_BASE_URL: process.env.REACT_WEBAPP_BASE_URL,
+};
+
+const constants = {
   ORDER_STATUS: {
     NEW: 'NEW',
     DONE: 'DONE',
     CANCELLED: 'CANCELLED',
     IN_PROGRESS: 'IN PROGRESS',
   },
+};
+
+module.exports = {
+  ...envVars,
+  ...constants,
 };
