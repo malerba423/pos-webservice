6e10289b6e7f32885d98d4c292dc2a6156b820ea
malerba423 <malerba423@gmail.com>
2021-05-24 15:14:10 -0700

    updates for elastic beanstalk

--- src/config.js
+++ src/config.js
@@ -1,7 +1,19 @@
 const dotenv = require('dotenv');
-const envVars = dotenv.config({ path: `${__dirname}/../.env` }).parsed;
+dotenv.config();
 
-const configVars = {
+module.exports = {
+  PORT: '3001',
+  NODE_ENV: 'local_development',
+  DATABASE_URL: 'postgresql://postgres:postgres@localhost:5432/postgres',
+  STRIPE_PUBLIC_KEY:
+    'pk_test_51Fa7TWKyucmbfgO2k9j41LfOVCR0YDjQDWJZsv75aaIRFCCATKhc7zBg30jKwQjOdK57yH8Qx1ts6MIxQWtTKxOx00TLgOt60u',
+  STRIPE_SECRET_KEY:
+    'sk_test_51Fa7TWKyucmbfgO2ccZajIygKaIYqz1zymEYKM7sgpaCZmP41qSeIejldlE6bEoSNnkRLg0jauV5ZsjtJq8ZKEol00YrIvFNB9',
+  SALES_TAX_RATE: '0.087',
+  JWT_SECRET: 'some_test_secret_or_something',
+  JWT_ALGORITHM: 'HS256',
+  DEV_HOST: 'trbikes.kitchen.local',
+  REACT_WEBAPP_BASE_URL: 'http://localhost:3000',
   ORDER_STATUS: {
     NEW: 'NEW',
     DONE: 'DONE',
@@ -9,5 +21,3 @@ const configVars = {
     IN_PROGRESS: 'IN PROGRESS',
   },
 };
-
-module.exports = { ...configVars, ...envVars };
--- src/serverstart.js
+++
@@ -1,4 +0,0 @@
-const app = require('./server');
-const { PORT } = require('./config');
-
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
